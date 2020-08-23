c4df0ec425a663419b5fb07e5b97547746c8bb64
Hitalo <40531911+HitaloSama@users.noreply.github.com>
2020-08-23 10:05:09 -0300

    Replaced ErfanGSI with TrebleEx in the img file to identify that GSI was made by @ TrebleExperience (on Telegram)

--- make.sh
+++ make.sh
@@ -192,7 +192,7 @@ if [ "$outputtype" == "Aonly" ]; then
 fi
 
 date=`date +%Y%m%d`
-outputname="$romtypename-$outputtype-$sourcever-$date-ErfanGSI"
+outputname="$romtypename-$outputtype-$sourcever-$date-TrebleEx"
 outputimagename="$outputname".img
 outputtextname="$outputname".txt
 if [ "$4" == "" ]; then
